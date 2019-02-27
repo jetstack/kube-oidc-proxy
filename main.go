8922ba3fd2992a40220d1cb721ec8f6ed3925641
JoshVanL <vleeuwenjoshua@gmail.com>
2019-02-27 19:52:06 +0000

    Use httputil.ServeProxy hand better handle unauthed
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- main.go
+++ main.go
@@ -3,32 +3,38 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
-	"io"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
-	"os"
+	"time"
 
+	"github.com/sirupsen/logrus"
 	"k8s.io/apiserver/pkg/authentication/request/bearertoken"
 	"k8s.io/apiserver/plugin/pkg/authenticator/token/oidc"
 )
 
+var (
+	errUnauthorized = errors.New("Unauthorized")
+)
+
 type Proxy struct {
-	//req authenticator.Request
+	tlsTransport *http.Transport
+	reqAuther    *bearertoken.Authenticator
 }
 
 func main() {
 	cert, err := tls.LoadX509KeyPair("client.crt", "client.key")
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 
 	// Load CA cert
 	caCert, err := ioutil.ReadFile("client.ca")
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 
 	caCertPool := x509.NewCertPool()
@@ -40,8 +46,6 @@ func main() {
 		RootCAs:      caCertPool,
 	}
 	tlsConfig.BuildNameToCertificate()
-	transport := &http.Transport{TLSClientConfig: tlsConfig}
-	client := &http.Client{Transport: transport}
 
 	config := oidc.Options{
 		IssuerURL:     "https://accounts.google.com",
@@ -51,49 +55,26 @@ func main() {
 
 	iodcAuther, err := oidc.New(config)
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 	reqAuther := bearertoken.New(iodcAuther)
+	logrus.Infof("waiting for oidc provider to become ready...")
+	time.Sleep(10 * time.Second)
+	logrus.Infof("proxy ready")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, ok, err := reqAuther.AuthenticateRequest(r)
-		if err != nil {
-			log.Printf("Unable to authenticate the request due to an error: %v", err)
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("Unauthorized"))
-			return
-		}
-		if !ok {
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("Unauthorized"))
-			return
-		}
+	url, err := url.Parse("https://api.jvl-cluster.develop.tarmak.org")
+	if err != nil {
+		logrus.Fatalf("failed to parse url: %s", err)
+	}
 
-		URL, err := url.Parse("https://api.jvl-cluster.develop.tarmak.org" + r.URL.Path)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "parse error: "+err.Error()+"\n")
-			return
-		}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	p := &Proxy{transport, reqAuther}
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	//proxy.Transport = transport
+	proxy.Transport = p
+	proxy.ErrorHandler = p.ErrorHandler
 
-		req := &http.Request{
-			URL:  URL,
-			Body: r.Body,
-		}
-		req.Header = r.Header
-
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "do error: "+err.Error()+"\n")
-			return
-		}
-
-		setHeader(w, res.Header)
-		w.WriteHeader(res.StatusCode)
-		io.Copy(w, res.Body)
-		res.Body.Close()
-	})
-
-	err = http.ListenAndServeTLS(":8000", "apiserver.crt", "apiserver.key", nil)
+	err = http.ListenAndServeTLS(":8000", "apiserver.crt", "apiserver.key", proxy)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -105,4 +86,35 @@ func setHeader(w http.ResponseWriter, src http.Header) {
 			w.Header().Add(k, v)
 		}
 	}
+}
+
+func duplicateHeader(o http.Header) http.Header {
+	n := http.Header{}
+	for k, v := range o {
+		n[k] = v
+	}
+
+	return n
+}
+
+func (p *Proxy) ErrorHandler(rw http.ResponseWriter, r *http.Request, err error) {
+	if err == errUnauthorized {
+		rw.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	rw.WriteHeader(http.StatusBadGateway)
+}
+
+func (p *Proxy) RoundTrip(r *http.Request) (*http.Response, error) {
+	_, ok, err := p.reqAuther.AuthenticateRequest(r)
+	if err != nil {
+		logrus.Errorf("Unable to authenticate the request due to an error: %v", err)
+		return nil, errUnauthorized
+	}
+	if !ok {
+		return nil, errUnauthorized
+	}
+
+	return p.tlsTransport.RoundTrip(r)
 }
