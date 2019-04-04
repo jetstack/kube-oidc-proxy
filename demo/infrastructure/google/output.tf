ae57f6b5c6f429138ef05d3dc2c1d72005a34941
JoshVanL <vleeuwenjoshua@gmail.com>
2019-04-04 10:24:45 +0100

    Consolidates gangway and oidc client secret to config.json
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- demo/infrastructure/google/output.tf
+++ demo/infrastructure/google/output.tf
@@ -2,9 +2,7 @@ locals {
   config = {
     cert_manager = "${module.dns.config}"
     externaldns  = "${module.dns.config}"
-  }
 
-  secrets = {
     oidc_client_secret = "${module.secrets.oidc_client_secret}"
     gangway_key        = "${module.secrets.gangway_key}"
   }
@@ -17,8 +15,4 @@ output "config" {
 # This fetches KUBECONFIG and grants full cluster admin access to the current user
 output "kubeconfig_command" {
   value = "gcloud container clusters get-credentials ${module.cluster.name} --zone ${var.google_zone} --project ${module.cluster.project} && kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account) --dry-run -o yaml | kubectl apply -f -"
-}
-
-output "secrets" {
-  value = "${jsonencode(local.secrets)}"
 }
