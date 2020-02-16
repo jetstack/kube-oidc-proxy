eb999db2fb9f21e690a1ccfe537eda344299a2ec
saiteja313 <saiteja313@gmail.com>
2020-02-16 12:00:10 -0500

    fix cluster auth for k8s provider
    Signed-off-by: saiteja313 <saiteja313@gmail.com>

--- demo/infrastructure/modules/amazon-cluster/cluster.tf
+++ demo/infrastructure/modules/amazon-cluster/cluster.tf
@@ -114,3 +114,19 @@ module "eks" {
 
   worker_additional_security_group_ids = ["${aws_security_group.all_worker_mgmt.id}"]
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.10"
+}
