3a08ebcbc5365baf1a124dd9c40ec4ab90c035f1
mlbiam <marc@tremolo.io>
2021-12-14 09:00:18 -0500

    for #3 removing unneeded generate

--- pkg/mocks/mocks.go
+++ pkg/mocks/mocks.go
@@ -4,4 +4,3 @@ package mocks
 // This package contains generated mocks
 
 //go:generate mockgen -package=mocks -destination authenticator.go k8s.io/apiserver/pkg/authentication/authenticator Token
-//go:generate mockgen -package=mocks -destination subjectaccessreviewer.go k8s.io/api/authorization/v1 SubjectAccessReview
